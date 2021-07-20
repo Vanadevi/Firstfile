fab10fa39b99fa59f407941aceedb6af289249cc
Vanadevi <vanadevi123@gmail.com>
2021-07-20 07:46:42 -0700

    Task completed

---
+++ ArrayProgram.java
@@ -0,0 +1,19 @@
+package org.array;
+
+public class ArrayProgram {
+	
+	static int a[]= {10,20,30,40,50};
+	
+
+public static void main(String[] args) {
+
+   System.out.println(a[4]);
+   
+	}
+	
+	
+	
+	
+			
+
+}
